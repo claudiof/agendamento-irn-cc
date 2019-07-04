d0a205a39ee79c81e7ebdc9271e4266298d68802
Cláudio Franco <claudio@claudiofranco.pt>
2019-07-04 11:07:12 +0100

    analytics

--- index.php
+++ index.php
@@ -50,5 +50,16 @@
             } );
         } );
     </script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="//www.googletagmanager.com/gtag/js?id=UA-143298329-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-143298329-1');
+    </script>
+
   </body>
 </html>
