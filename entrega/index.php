6222c4a87208c1c71a43ea3742bb2730d4994db7
Cláudio Franco <claudio@claudiofranco.pt>
2019-07-10 09:59:18 +0100

    datas de entrega

--- entrega/index.php
+++ entrega/index.php
@@ -52,6 +52,8 @@
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 
     <script>
         $(document).ready(function() {
